5fbdf322d7c23addf3f925c5ad884036f75047ab
viet trieu <61877838+viettrieu1501@users.noreply.github.com>
2024-10-30 00:53:38 +0700

    fix: tag b in dap an tu luan

--- src/controllers/EvaluateController.js
+++ src/controllers/EvaluateController.js
@@ -301,9 +301,10 @@ const download = async (req, res) => {
                     selectedAnswer.cau_hoi.loai_cau_hoi === 0 &&
                     selectedAnswer.noi_dung_tra_loi &&
                     selectedAnswer.cau_hoi.dap_ans[0].noi_dung_dap_an &&
-                    selectedAnswer.noi_dung_tra_loi.trim().toLowerCase() ==
+                    selectedAnswer.noi_dung_tra_loi.trim().replaceAll('<b>', '').replaceAll('</b>', '').replaceAll('<em>', '').replaceAll('</em>', '').replaceAll('<u>', '').replaceAll('</u>', '').toLowerCase() ==
                         selectedAnswer.cau_hoi.dap_ans[0].noi_dung_dap_an
                             .trim()
+                            .replaceAll('<b>', '').replaceAll('</b>', '').replaceAll('<em>', '').replaceAll('</em>', '').replaceAll('<u>', '').replaceAll('</u>', '')
                             .toLowerCase()
                 )
                     result = true;
--- src/controllers/StudentExamController.js
+++ src/controllers/StudentExamController.js
@@ -479,9 +479,10 @@ const putUpdate = async (req, res) => {
                 selectedAnswer.cau_hoi.loai_cau_hoi === 0 &&
                 selectedAnswer.noi_dung_tra_loi &&
                 selectedAnswer.cau_hoi.dap_ans[0].noi_dung_dap_an &&
-                selectedAnswer.noi_dung_tra_loi.trim().toLowerCase() ==
+                selectedAnswer.noi_dung_tra_loi.trim().replaceAll('<b>', '').replaceAll('</b>', '').replaceAll('<em>', '').replaceAll('</em>', '').replaceAll('<u>', '').replaceAll('</u>', '').toLowerCase() ==
                     selectedAnswer.cau_hoi.dap_ans[0].noi_dung_dap_an
                         .trim()
+                        .replaceAll('<b>', '').replaceAll('</b>', '').replaceAll('<em>', '').replaceAll('</em>', '').replaceAll('<u>', '').replaceAll('</u>', '')
                         .toLowerCase()
             ) {
                 result = true;
@@ -642,9 +643,10 @@ const putUpdatev2 = async (req, res) => {
                 selectedAnswer.cau_hoi.loai_cau_hoi === 0 &&
                 selectedAnswer.noi_dung_tra_loi &&
                 selectedAnswer.cau_hoi.dap_ans[0].noi_dung_dap_an &&
-                selectedAnswer.noi_dung_tra_loi.trim().toLowerCase() ==
+                selectedAnswer.noi_dung_tra_loi.trim().replaceAll('<b>', '').replaceAll('</b>', '').replaceAll('<em>', '').replaceAll('</em>', '').replaceAll('<u>', '').replaceAll('</u>', '').toLowerCase() ==
                     selectedAnswer.cau_hoi.dap_ans[0].noi_dung_dap_an
                         .trim()
+                        .replaceAll('<b>', '').replaceAll('</b>', '').replaceAll('<em>', '').replaceAll('</em>', '').replaceAll('<u>', '').replaceAll('</u>', '')
                         .toLowerCase()
             ) {
                 if (selectedAnswer.cau_hoi.chuyen_nganh_id === 1) {
